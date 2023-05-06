1ae444b1c4dad045a44aafbc171584494e4bcfc4
Bo Yuan <93609557+boyuan99@users.noreply.github.com>
2023-05-06 18:44:07 -0400

    Initial commit
    Add main page

---
+++ script.js
@@ -0,0 +1,42 @@
+let history = [];
+
+function adjustHeight() {
+    const textArea = document.getElementById("textArea");
+    textArea.style.height = 'auto';
+    textArea.style.height = (textArea.scrollHeight) + 'px';
+}
+
+function removeLineBreaks() {
+    const textArea = document.getElementById("textArea");
+    addToHistory(textArea.value);
+    const stringWithoutLineBreaks = textArea.value.replace(/(\r\n|\n|\r)/gm, " ");
+    textArea.value = stringWithoutLineBreaks;
+    adjustHeight();
+}
+
+function removeLineNumbers() {
+    const textArea = document.getElementById("textArea");
+    addToHistory(textArea.value);
+    const stringWithoutLineNumbers = textArea.value.replace(/^[\s]*\d+\s*(?=.)/gm, (match) => match.replace(/\d+/g, ''));
+    textArea.value = stringWithoutLineNumbers;
+    adjustHeight();
+}
+
+function addToHistory(text) {
+    if (history.length === 10) {
+        history.shift();
+    }
+    history.push(text);
+}
+
+function undo() {
+    if (history.length > 0) {
+        const textArea = document.getElementById("textArea");
+        history.pop(); // Remove the current state
+        const previousState = history.length > 0 ? history[history.length - 1] : '';
+        textArea.value = previousState;
+        adjustHeight();
+    }
+}
+
+adjustHeight();
